d19a4e029bc5d2cd9e527157e351a5d373301d88
117067 <50777048+117067@users.noreply.github.com>
2022-12-23 10:07:58 -0500

    Update Simulation.java

--- Simulation.java
+++ Simulation.java
@@ -2,6 +2,7 @@ import java.util.Random;
 
 class Simulation {
   public static void main(String[] args) {
+    //edit the result values as necessary
     int smallerResult = 16;
     int largerResult = 20;
     int trials = 1000000;

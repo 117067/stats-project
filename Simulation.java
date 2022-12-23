2722232a9c4135c3d71d58c8136856294e81918e
117067 <50777048+117067@users.noreply.github.com>
2022-12-23 08:21:09 -0500

    Create Simulation.java

---
+++ Simulation.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+class Simulation {
+  public static void main(String[] args) {
+    int smallerResult = 16;
+    int largerResult = 20;
+    int trials = 1000000;
+    Random random = new Random();
+    int sum = smallerResult + largerResult;
+    int successes = 0;
+    for (int i = 0; i < trials; i ++) {
+      int result = 0;
+      for (int j = 0; j < sum; j ++) {
+        if (random.nextBoolean()) result ++;
+      }
+      if (result <= smallerResult) successes ++;
+    }
+    System.out.println(((double) successes) / ((double) trials));
+  }
+}
